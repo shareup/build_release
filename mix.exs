570622b69b047f6c28e411a9d0bd35d08bf96a59
Nathan Herald <me@nathanherald.com>
2020-05-31 20:22:32 +0200

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule BuildRelease.Mixfile do
   def project do
     [
       app: :build_release,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.2.0",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
